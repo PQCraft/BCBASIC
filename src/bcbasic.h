c81e262046ddc824f334784f7631fa14b5547ce2
PQCraft <0456523@gmail.com>
2022-05-05 22:44:20 -0400

    Alpha 0.1.4
    Improvements to the file loader

--- src/bcbasic.h
+++ src/bcbasic.h
@@ -1,9 +1,9 @@
 #ifndef BCB_H
 #define BCB_H
 
-#define BCB_VERSION "0.1.3"
+#define BCB_VERSION "0.1.4"
 #define BCB_BUILD "Alpha"
-#define BCB_BUILD_ID 2022050300
+#define BCB_BUILD_ID 2022050500
 
 #include "common.h"
 

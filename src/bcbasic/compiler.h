4b06b4fad3a07e0678aefc35c0358417db32e93a
PQCraft <0456523@gmail.com>
2025-04-15 23:47:57 -0400

    Small update

--- src/bcbasic/compiler.h
+++ src/bcbasic/compiler.h
@@ -5,18 +5,47 @@
 #include <stddef.h>
 #include "error.h"
 
-struct BCB_PreprocMacro {
+// Macros
+enum BCB_MacroDefType {
+    BCB_MACRODEF_OBJLIKE,   // Object-like macro
+    BCB_MACRODEF_FUNCLIKE   // Function-like macro
+};
+struct BCB_MacroDef {
+    enum BCB_MacroDefType type;
     char* name;
-    char* value;
     uint32_t namecrc;
+    union {
+        struct {
+            char* text;
+        } objlike;
+        struct {
+            struct {
+                size_t count;
+                char* names;
+            } args;
+            char* text;
+        } funclike;
+    };
 };
 
-// Fills in the CRC values of the 'name' members
-void BCB_PrepPreprocMacros(struct BCB_PreprocMacro*, size_t ct);
+// Validates names and fills in CRC values
+int BCB_PrepMacroDefs(struct BCB_MacroDef*, size_t count, unsigned flags);
+#define BCB_PREPMACRODEFS_SKIPVALIDATION (1 << 0)   // Skip validating names if you already know they are valid
 
+// Get a list of extensions the compiler supports
+char** BCB_GetCompilerExtensions(void);
+
+// Compiler options
 struct BCB_CompilerOpt {
-    size_t macroct;
-    struct BCB_PreprocMacro* macros;
+    struct {
+        struct BCB_MacroDef* data;
+        size_t count;
+    } macros;
+    struct {
+        char** claim;       // Claim these extensions are available (NULL to use 'enable' and 'ondemand')
+        char** enable;      // Enable these extensions
+        char** ondemand;    // Enable these extensions only if `#require`d
+    } extensions;
 };
 
 // Callback type for the compiler to request program text
--- src/bcbasic/vm.h
+++ src/bcbasic/vm.h
@@ -1,6 +1,7 @@
 #ifndef BCBASIC_VM_H
 #define BCBASIC_VM_H
 
-// ...
+// Get a list of extensions the VM has
+char** BCB_GetVMExtensions(void);
 
 #endif
